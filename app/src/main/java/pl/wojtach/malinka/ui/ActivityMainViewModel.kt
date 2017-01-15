8a3efca9272670eb69f2d9e5545d5859949fc524
Lukasz <circusmagnus@gmail.com>
2017-01-15 17:57:33 +0100

    Pull-to-refresh implemented.

--- app/src/main/java/pl/wojtach/malinka/ui/ActivityMainViewModel.kt
+++ app/src/main/java/pl/wojtach/malinka/ui/ActivityMainViewModel.kt
@@ -1,6 +1,8 @@
 package pl.wojtach.malinka.ui
 
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import pl.wojtach.malinka.data.RetrofitProvider
 import pl.wojtach.malinka.data.SensorRepositoryRetrofit
 import pl.wojtach.malinka.databinding.ActivityMainBinding
@@ -11,7 +13,9 @@ import rx.schedulers.Schedulers
 /**
  * Created by Lukasz on 14.01.2017.
  */
-class ActivityMainViewModel(val boundView: ActivityMainBinding) {
+class ActivityMainViewModel(val boundView: ActivityMainBinding) : SwipeRefreshLayout.OnRefreshListener {
+
+    val TAG = ActivityMainViewModel::class.java.simpleName
 
     val sensors: MutableList<Sensor> = mutableListOf()
 
@@ -19,6 +23,7 @@ class ActivityMainViewModel(val boundView: ActivityMainBinding) {
         val layoutManager = LinearLayoutManager(boundView.activityMain.context)
         boundView.sensorList.setLayoutManager(layoutManager)
         boundView.sensorList.adapter = SensorAdapter(sensors)
+        boundView.swipeRefreshLayout.setOnRefreshListener(this)
     }
 
     fun refreshSensorList() {
@@ -26,10 +31,18 @@ class ActivityMainViewModel(val boundView: ActivityMainBinding) {
                 .getInfoFromSensors()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe {
+                .subscribe({
                     sensors.clear()
                     sensors.addAll(it)
                     boundView.sensorList.adapter.notifyDataSetChanged()
-                }
+                    boundView.swipeRefreshLayout.isRefreshing = false
+                }, { error ->
+                    boundView.swipeRefreshLayout.isRefreshing = false
+                    Log.d(TAG, "can`t load data: " + error.message)
+                })
+    }
+
+    override fun onRefresh() {
+        refreshSensorList()
     }
 }
