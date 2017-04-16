b709504dd3821e8fc70838c2a4934465023aef58
Lukasz Wojtach <circusmagnus@gmail.com>
2017-04-16 21:38:54 +0200

    More package refactoring

--- app/src/main/kotlin/pl/wojtach/malinka/data/sensors/SensorRepositoryRetrofit.kt
+++ app/src/main/kotlin/pl/wojtach/malinka/data/sensors/SensorRepositoryRetrofit.kt
@@ -1,10 +1,11 @@
 package pl.wojtach.malinka.data.sensors
 
-import pl.wojtach.malinka.entities.Sensor
-import pl.wojtach.malinka.entities.SensorLocation
-import pl.wojtach.malinka.data.sensors.SensorRepository
 import io.reactivex.Observable
 import pl.wojtach.malinka.data.RetrofitProvider
+import pl.wojtach.malinka.entities.Sensor
+import pl.wojtach.malinka.entities.SensorLocation
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 /**
  * Created by Lukasz on 08.01.2017.
@@ -19,4 +20,18 @@ class SensorRepositoryRetrofit(val sensorDataProvider: RetrofitProvider) : Senso
     override fun getInfoFromSensors(location: SensorLocation): Observable<List<Sensor>> {
         return sensorDataProvider.dataProvider.getSensors()
     }
+}
+
+/**
+ * Created by Lukasz on 08.01.2017.
+ */
+interface SensorDataProvider {
+
+    @GET("lastStatus")
+    fun getSensors(): Observable<List<Sensor>>
+
+    @GET("setDeviceStatus")
+    fun setDeviceStatus(@Query("sensor") macAddres: String,
+                        @Query("type") type: Int,
+                        @Query("status") isActive: Int): Observable<Void>
 }
