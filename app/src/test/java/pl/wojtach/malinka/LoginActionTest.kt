f13829c3d912fe36f3a4ef395b9455ce1ae93b71
Lukasz Wojtach <circusmagnus@gmail.com>
2017-05-05 21:11:18 +0200

    Add TODO for SPEK

--- app/src/test/java/pl/wojtach/malinka/LoginActionTest.kt
+++ app/src/test/java/pl/wojtach/malinka/LoginActionTest.kt
@@ -6,9 +6,11 @@ import org.junit.Test
  * Created by lukaszwojtach on 05.05.2017.
  */
 class LoginActionTest {
+
+    //TODO install spek from: https://medium.com/@jozemberi/developing-with-kotlin-and-testing-with-spek-d69a94857d
     @Test
     fun transformState() {
-        
+
     }
 
 }
