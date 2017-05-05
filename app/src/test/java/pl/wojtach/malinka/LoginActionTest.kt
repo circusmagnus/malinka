7709141fd9ca10c3ae8ff4a61592dc2be3697558
Lukasz Wojtach <circusmagnus@gmail.com>
2017-05-05 21:08:25 +0200

    Test for login action in need of spek

---
+++ app/src/test/java/pl/wojtach/malinka/LoginActionTest.kt
@@ -0,0 +1,14 @@
+package pl.wojtach.malinka
+
+import org.junit.Test
+
+/**
+ * Created by lukaszwojtach on 05.05.2017.
+ */
+class LoginActionTest {
+    @Test
+    fun transformState() {
+        
+    }
+
+}
