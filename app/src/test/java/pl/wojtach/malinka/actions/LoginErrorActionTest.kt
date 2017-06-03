a9aba423612ee4093280225c077cabbbbfd239a0
Lukasz Wojtach <circusmagnus@gmail.com>
2017-06-04 01:33:44 +0200

    Remove Spek
    .. as it does not work reliably. Rewrite test in junit

--- app/src/test/java/pl/wojtach/malinka/actions/LoginErrorActionTest.kt
+++ app/src/test/java/pl/wojtach/malinka/actions/LoginErrorActionTest.kt
@@ -1,48 +1,53 @@
 package pl.wojtach.malinka.actions
 
-import org.amshove.kluent.shouldContain
-import org.amshove.kluent.shouldEqualTo
-import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.given
-import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.api.dsl.on
-import org.junit.platform.runner.JUnitPlatform
-import org.junit.runner.RunWith
-import pl.wojtach.malinka.state.ErrorState
+import org.amshove.kluent.shouldEqual
+import org.junit.Before
+import org.junit.Test
+import pl.wojtach.malinka.state.State
 import pl.wojtach.malinka.state.StateMachine
 import pl.wojtach.malinka.state.createInitialState
 
 /**
- * Created by lukaszwojtach on 31.05.2017.
+ * Created by lukaszwojtach on 04.06.2017.
  */
+class LoginErrorActionTest {
+    lateinit var stateMachine: StateMachine<State>
+    val invalidLoginMessage = "invalid login"
+    val secondErrorMessage = "server failure"
 
-@RunWith(JUnitPlatform::class)
-class LoginErrorActionTest : Spek({
-    given("No previous errors", {
-        val stateMachine = StateMachine.getInstance(createInitialState())
-        on("action signalling an login error", {
-            val error = "failed to log in"
-            stateMachine.dispatch(LoginErrorAction(error))
-            it("should create ErrorState with single entry containing its message", {
-                stateMachine.getState().errorState?.errorMessages ?: emptySet<String>() shouldContain error
-            })
-        })
-    })
-    given("Previous error", {
-        val previousError = "login error"
-        val stateMachine = StateMachine
-                .getInstance(createInitialState()
-                        .copy(errorState = ErrorState(errorMessages = setOf(previousError))))
-        on("action signalling the same error", {
-            val nextError = "login error"
-            stateMachine.dispatch(LoginErrorAction(nextError))
-            it("should not add another error", {
-                stateMachine.getState().errorState?.errorMessages?.size ?: 0 shouldEqualTo 3
-            })
-            it("should retain previous error", {
-                stateMachine.getState().errorState?.errorMessages ?: emptySet<String>() shouldContain previousError
-            })
-        })
-    })
-})
+    @Before
+    fun setUp() {
+        stateMachine = StateMachine.getInstance(createInitialState())
+    }
 
+    @Test
+    fun firstErrorSinceBeginning() {
+        stateMachine.dispatch(LoginErrorAction(invalidLoginMessage))
+
+        val result = stateMachine.getState()
+
+        result.errorState?.errorMessages shouldEqual setOf(invalidLoginMessage)
+    }
+
+    @Test
+    fun secondErrorSinceBeginning() {
+        stateMachine.dispatch(LoginErrorAction(invalidLoginMessage))
+        stateMachine.dispatch(LoginErrorAction(secondErrorMessage))
+
+        val result = stateMachine.getState()
+
+        result.errorState?.errorMessages shouldEqual setOf(invalidLoginMessage, secondErrorMessage)
+    }
+
+    @Test
+    fun thirdErrorSameAsFirst() {
+        stateMachine.dispatch(LoginErrorAction(invalidLoginMessage))
+        stateMachine.dispatch(LoginErrorAction(secondErrorMessage))
+        stateMachine.dispatch(LoginErrorAction(invalidLoginMessage))
+
+        val result = stateMachine.getState()
+
+        result.errorState?.errorMessages shouldEqual setOf(invalidLoginMessage, secondErrorMessage)
+    }
+
+}
