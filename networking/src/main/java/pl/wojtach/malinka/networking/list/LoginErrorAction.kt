75ad15d1de9aa3340588dcbdc719e6340da06214
Lukasz <circusmagnus@gmail.com>
2017-11-02 22:28:02 +0100

    More fixes in gradle

--- networking/src/main/java/pl/wojtach/malinka/networking/list/LoginErrorAction.kt
+++ networking/src/main/java/pl/wojtach/malinka/networking/list/LoginErrorAction.kt
@@ -1,10 +1,14 @@
 package pl.wojtach.malinka.networking.list
 
+import pl.wojtach.malinka.statemachine.Action
+import pl.wojtach.malinka.statemachine.states.PHASE.FAILED
+import pl.wojtach.malinka.statemachine.states.State
 
-internal class LoginErrorAction(val errorMessage: String) : pl.wojtach.malinka.statemachine.Action<pl.wojtach.malinka.statemachine.states.State> {
-    override fun transformState(oldState: pl.wojtach.malinka.statemachine.states.State): pl.wojtach.malinka.statemachine.states.State {
+
+internal class LoginErrorAction(val errorMessage: String) : Action<State> {
+    override fun transformState(oldState: State): State {
         return oldState.copy(
-                loginState = oldState.loginState.copy(phaseOfLogging = pl.wojtach.malinka.statemachine.states.PHASE.FAILED),
+                loginState = oldState.loginState.copy(phaseOfLogging = FAILED),
                 errorState = pl.wojtach.malinka.statemachine.states.ErrorState(createNewErrorSet(oldState.errorState?.errorMessages))
         )
     }
