145281f02118f71a16cf18728e42a43345ad77de
Lukasz Wojtach <lukasz.wojtach@allegro.pl>
2018-05-04 18:40:28 +0200

    Camera Fragment ready in simplest version

--- camera/src/main/java/pl/wojtach/camera/CameraFragment.kt
+++ camera/src/main/java/pl/wojtach/camera/CameraFragment.kt
@@ -5,33 +5,41 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.MediaController
 import com.google.firebase.storage.FirebaseStorage
 import com.google.android.gms.tasks.OnFailureListener
 import com.google.firebase.storage.FileDownloadTask
 import com.google.android.gms.tasks.OnSuccessListener
+import kotlinx.android.synthetic.main.fragment_video.*
+import kotlinx.android.synthetic.main.fragment_video.view.*
 import java.io.File
 
 
 class CameraFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        val storageRef = FirebaseStorage.getInstance().reference.child("lot1/20180504_122259D.mp4")
-
-        val localFile = File.createTempFile("images", "jpg")
-
-        storageRef.getFile(localFile)
-                .addOnSuccessListener(OnSuccessListener<FileDownloadTask.TaskSnapshot> {
-                    // Successfully downloaded data to local file
-                    // ...
-                }).addOnFailureListener(OnFailureListener {
-                    // Handle failed download
-                    // ...
-                })
+    private val videoUrl by lazy {
+        arguments?.getString(URL_KEY) ?: ""
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+            inflater.inflate(R.layout.fragment_video, container, false)
+                    .apply {
+                        with(alert_video) {
+                            setMediaController(MediaController(activity))
+                            setVideoPath(videoUrl)
+                        }
+                    }
+
+    override fun onResume() {
+        super.onResume()
+        alert_video.start()
     }
+
+    companion object {
+        const val URL_KEY = "url key"
+
+        fun newInstance(videoUrl: String): CameraFragment =
+                CameraFragment().apply { arguments = Bundle().apply { putString(URL_KEY, videoUrl) } }
+    }
+
 }
