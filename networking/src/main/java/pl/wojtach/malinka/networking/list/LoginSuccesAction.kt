727618d77ee2844bf2dd9a689a6c6d6babe0acc1
Lukasz Wojtach <lukasz.wojtach@allegro.pl>
2018-09-29 12:09:31 +0200

    Sort last alerts on sensor

--- networking/src/main/java/pl/wojtach/malinka/networking/list/LoginSuccesAction.kt
+++ networking/src/main/java/pl/wojtach/malinka/networking/list/LoginSuccesAction.kt
@@ -2,29 +2,46 @@ package pl.wojtach.malinka.networking.list
 
 import pl.wojtach.malinka.networking.WsAlert
 import pl.wojtach.malinka.networking.WsSensor
-import pl.wojtach.malinka.networking.convertToEntity
+import pl.wojtach.malinka.networking.toEntitySensor
 import pl.wojtach.malinka.statemachine.Action
 import pl.wojtach.malinka.statemachine.entities.Sensor
 import pl.wojtach.malinka.statemachine.states.PHASE
 import pl.wojtach.malinka.statemachine.states.SensorState
 import pl.wojtach.malinka.statemachine.states.State
 
-
 internal class LoginSuccesAction(val sensors: List<WsSensor>, val alerts: List<WsAlert>) : Action<State> {
     override fun transformState(oldState: State): State {
         return oldState.copy(
-                loginState = oldState.loginState.copy(phaseOfLogging = PHASE.FINISHED),
-                sensorState = SensorState(phase = PHASE.FINISHED, sensors = addAlertsToSensors()
-                )
+            loginState = oldState.loginState.copy(phaseOfLogging = PHASE.FINISHED),
+            sensorState = SensorState(
+                phase = PHASE.FINISHED, sensors = addAlertsToSensors()
+            )
         )
     }
 
-    private fun addAlertsToSensors(): List<Sensor> = sensors.convertToEntity().map { sensor ->
-        alerts.filter { alert -> doesBelongToSensor(sensor, alert) }
-                .let { sensor.copy(valueChanges = it.map { it.date }) }
-    }
+    private fun addAlertsToSensors(): List<Sensor> = sensors
+        .asSequence()
+        .map { toEntitySensor(it) }
+        .map { sensor -> addAlertsToSensor(sensor) }
+        .toList()
+
+    private fun addAlertsToSensor(sensor: Sensor): Sensor = alerts
+        .filter { alert -> doesBelongToSensor(sensor, alert) }
+        .let { alerts -> sensor.copy(valueChanges = getDatesAndSort(alerts)) }
+
+    private fun getDatesAndSort(alerts: List<WsAlert>) =
+        alerts.asSequence().map { it.date }.sortedByDescending { it }.toList()
+
+//        sensors
+//        .convertToEntity()
+//        .map { sensor ->
+//            alerts.filter { alert -> doesBelongToSensor(sensor, alert) }
+//                .let { alert ->
+//                    sensor.copy(valueChanges = alert.asSequence().map { it.date }.sortedByDescending { it }.toList())
+//                }
+//        }
 
     private fun doesBelongToSensor(sensor: Sensor, alert: WsAlert) =
-            sensor.mac == alert.mac && sensor.type == 2
+        sensor.mac == alert.mac && sensor.type == 2
 }
 
--- networking/src/main/java/pl/wojtach/malinka/networking/networkingUtils.kt
+++ networking/src/main/java/pl/wojtach/malinka/networking/networkingUtils.kt
@@ -15,14 +15,19 @@ internal fun StateMachine<State>.getUser() = getState().loginState.currentUser
 internal fun StateMachine<State>.getBaseUrl() = getState().loginState.currentBaseUrl
 
 fun List<WsSensor>.convertToEntity() = this.map {
-    Sensor(
-            mac = it.mac ?: throw IOException("Critical field is null: Sensor.mac"),
-            name = it.name ?: "",
-            type = it.type ?: throw IOException("Critical field is null: Sensor.type"),
-            isActive = it.isActive ?: throw IOException("Critical field is null: Sensor.isActive"),
-            lastDate = it.lastDate ?: "",
-            lastValue = it.lastValue ?: "",
-            hasError = false,
-            shouldSync = false,
-            valueChanges = kotlin.collections.emptyList())
+        toEntitySensor(it)
+}
+
+fun toEntitySensor(wsSensor: WsSensor): Sensor {
+        return Sensor(
+                mac = wsSensor.mac ?: throw IOException("Critical field is null: Sensor.mac"),
+                name = wsSensor.name ?: "",
+                type = wsSensor.type ?: throw IOException("Critical field is null: Sensor.type"),
+                isActive = wsSensor.isActive ?: throw IOException("Critical field is null: Sensor.isActive"),
+                lastDate = wsSensor.lastDate ?: "",
+                lastValue = wsSensor.lastValue ?: "",
+                hasError = false,
+                shouldSync = false,
+                valueChanges = emptyList()
+        )
 }
